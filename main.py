7f98549e5653c0845b8a239a9e9d68d10369d95a
Pedro <pedrolucas_vasconcelos@hotmail.com>
2025-05-07 17:58:50 -0300

    final da aula 3

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<a href=''./alunos''>Hello, World!</a>'
+    return '<a href=''./contatos''>Hello, World!</a>'
 
 
-@app.route('/alunos')
-def alunos():
-    return '<p>Alunos</p>'
+@app.route('/contatos')
+def contatos():
+    return render_template('contatos.html')
 
 
 if __name__ == '__main__':
-    # Run the Flask app on port 3001
     app.run(port=3001, debug=True)
